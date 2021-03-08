24413508c10d133f60b076a4a8f140b33f737823
repl.it user <replituser@example.com>
2021-03-08 05:38:39 +0000

    Simple if else exercise

---
+++ main.py
@@ -0,0 +1,11 @@
+print("What is the first number?")
+first_number=input()
+print("What is the second number?")
+second_number=input()
+if(first_number>second_number):
+  print("First number is bigger!")
+else:
+  print("First number is equal or smaller than second number")
+  print("Done!")
+  
+
